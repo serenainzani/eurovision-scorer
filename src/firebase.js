94a4832b0d407b7ffaad03e8d61460849d4f1635
Serena Inzani <serenainzani@hotmail.com>
2024-05-11 15:48:59 +0100

    readded emulator

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCaMNdhE6aiFh36H-HoonoTt55MyUsFSFc",
@@ -12,3 +12,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+connectFirestoreEmulator(db, "localhost", 8080);
